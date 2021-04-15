853d3020b54f5157c758cc2c77487d0660d11fcf
douglas254 <dobara@kabarak.ac.ke>
2021-04-15 11:53:19 +0300

    random password generator using tkinter

---
+++ RandomPasswordGenerator.py
@@ -0,0 +1,103 @@
+# Python program to generate random
+# password using Tkinter module
+import random
+import pyperclip
+from tkinter import *
+from tkinter.ttk import *
+
+# Function for calculation of password
+
+
+def low():
+    entry.delete(0, END)
+
+    # Get the length of password
+    length = var1.get()
+
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
+    password = ""
+
+    # if strength selected is low
+    if var.get() == 1:
+        for i in range(0, length):
+            password = password + random.choice(lower)
+        return password
+
+    # if strength selected is medium
+    elif var.get() == 0:
+        for i in range(0, length):
+            password = password + random.choice(upper)
+        return password
+
+    # if strength selected is strong
+    elif var.get() == 3:
+        for i in range(0, length):
+            password = password + random.choice(digits)
+        return password
+    else:
+        print("Please choose an option")
+
+
+# Function for generation of password
+def generate():
+    password1 = low()
+    entry.insert(10, password1)
+
+
+# Function for copying password to clipboard
+def copy1():
+    random_password = entry.get()
+    pyperclip.copy(random_password)
+
+
+# Main Function
+
+# create GUI window
+window = Tk()
+var = IntVar()
+var1 = IntVar()
+
+# Title of your GUI window
+window.title("Random Password Generator")
+
+# create label and entry to show password generated
+Random_password = Label(window, text="Password",
+                        font=("Arial Bold", 10, "bold"))
+Random_password.grid(row=0)
+entry = Entry(window)
+entry.grid(row=0, column=1)
+
+# create label for length of password
+c_label = Label(window, text="Length", font=("Arial Bold", 10, "bold"))
+c_label.grid(row=1)
+
+# create Buttons Copy which will copy password to clipboard
+copy_button = Button(window, text="Copy", command=copy1)
+copy_button.grid(row=0, column=2)
+
+# Generate button which will generate the password
+generate_button = Button(window, text="Generate", command=generate)
+generate_button.grid(row=0, column=3)
+
+# Radio Buttons for deciding the strength of password
+# Default strength is Medium
+radio_low = Radiobutton(window, text="Low", variable=var, value=1)
+radio_low.grid(row=1, column=2, sticky='E')
+radio_middle = Radiobutton(window, text="Medium", variable=var, value=0)
+radio_middle.grid(row=1, column=3, sticky='E')
+radio_strong = Radiobutton(window, text="Strong", variable=var, value=3)
+radio_strong.grid(row=1, column=4, sticky='E')
+combo = Combobox(window, textvariable=var1)
+
+# Combo Box for length of your password
+combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
+                   17, 18, 19, 20, 21, 22, 23, 24, 25,
+                   26, 27, 28, 29, 30, 31, 32, 33, 34, 35, "Length")
+combo.current(0)
+combo.bind('<<ComboboxSelected>>')
+combo.grid(column=1, row=1)
+
+# start the GUI
+window.mainloop()
